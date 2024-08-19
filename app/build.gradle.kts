d0e47a907a86151fdcd0ee13521e3561a19c3b71
abbas_khan <abbasadrali@gmail.com>
2024-08-19 14:49:09 +0400

    [Clean Gallery] Add required dependencies and packages for clean architecture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,38 @@
+
+import com.android.build.api.dsl.Packaging
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
-    namespace = "com.avrioc.cleangallery"
+    namespace = "com.example.testgallaryapplication"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.avrioc.cleangallery"
+        applicationId = "com.example.testgallaryapplication"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    packaging {
+        resources.excludes.addAll(
+            listOf(
+                "META-INF/LICENSE.md",
+                "META-INF/LICENSE-notice.md",
+            )
+        )
+    }
+
+    buildFeatures {
+        dataBinding = true
     }
 
     buildTypes {
@@ -33,6 +51,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    // Allow references to generated code
+
 }
 
 dependencies {
@@ -42,7 +63,46 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.fragment.ktx)
+
+
+    //lib for Viewmodel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+
+    // lib for Kotlin Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // lib for Hilt
+    implementation(libs.hilt.android.v248)
+    implementation(libs.androidx.junit.ktx)
+    kapt(libs.hilt.compiler)
+
+    // lib for Glide for Loading and cashing images
+    implementation(libs.glide)
+    annotationProcessor(libs.compiler)
+
+    // lib for navigation component
+    implementation(libs.androidx.navigation.fragment.ktx.v277)
+    implementation(libs.androidx.navigation.ui.ktx.v277)
+
+    // lib for lottie effect
+    implementation(libs.lottie)
+
+    // lib for Material Dialog
+    implementation(libs.materialdialog)
+
+    // lib for pagination
+    implementation(libs.androidx.paging.runtime.ktx)
+
+    // lib for testing
+    testImplementation(libs.kotlinx.coroutines.test.v171)
+    testImplementation(libs.mockk) // or latest version
+    androidTestImplementation(libs.mockk.android)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
---
+++ app/src/main/java/com/avrioc/cleangallery/di/CleanGallery.kt
@@ -0,0 +1,8 @@
+package com.avrioc.cleangallery.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CleanGallery:Application() {
+}
